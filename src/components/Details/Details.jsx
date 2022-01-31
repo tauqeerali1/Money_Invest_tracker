5e99e1c4e23d52885ca969f662aed770f1aa339b
Tauqeer Ali <tauqeer.ali.9934@gmail.com>
2022-01-31 14:16:47 +0530

    Update Details.jsx

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Card, CardHeader, CardContent, Typography } from '@material-ui/core';
 import { Doughnut } from 'react-chartjs-2';
-
+import 'chart.js/auto';
 import useStyles from './styles';
 import useTransactions from '../../useTransactions';
 
